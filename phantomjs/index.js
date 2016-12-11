5044d22c53b7c380b4b0fa4ae508eb84e5d21e99
Eugene Brodsky <fupslot@gmail.com>
2016-12-12 00:08:19 +0200

    parsing meta using cheerio

--- phantomjs/index.js
+++ phantomjs/index.js
@@ -1,14 +1,28 @@
+const cheerio = require('cheerio');
 const phantom_request = require('./phantom_request');
 
 // 'https://fupslot.github.io/workshop-2016/media-queries/index.html',
 phantom_request(
   {
-    url: 'http://phantomjs.org/api/command-line.html'
+    url: 'https://fupslot.github.io/workshop-2016/media-queries/index.html'
   },
   (err, data) => {
     if (err) {
       return console.log(err);
     }
-    return console.log(data);
+    const dom = cheerio.load(data);
+
+    const attrs = {};
+
+    dom('meta').each(function(i, node) {
+      const propName = node.attribs.name || node.attribs.property;
+      if (propName && propName.startsWith('og:')) {
+        attrs[propName] = node.attribs.content;
+      }
+    });
+
+    console.log(attrs);
+
+    return null;
   }
 );
